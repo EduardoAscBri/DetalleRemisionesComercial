7801e8619e45235e393096cbb58e9179c2ae15e8
Eduardo Ascencio Bribiesca <eduardoascbri@outlook.com>
2019-12-16 13:59:17 -0600

    Initial commit

---
+++ DetalleRemisiones/Forms/Principal.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace DetalleRemisiones.Forms
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSerie = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFolio = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSerie
+            // 
+            this.txtSerie.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSerie.Location = new System.Drawing.Point(28, 46);
+            this.txtSerie.Name = "txtSerie";
+            this.txtSerie.Size = new System.Drawing.Size(100, 29);
+            this.txtSerie.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Serie";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(186, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Folio";
+            // 
+            // txtFolio
+            // 
+            this.txtFolio.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFolio.Location = new System.Drawing.Point(159, 46);
+            this.txtFolio.Name = "txtFolio";
+            this.txtFolio.Size = new System.Drawing.Size(100, 29);
+            this.txtFolio.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(28, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(231, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Salir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(28, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(231, 32);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Consultar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 198);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFolio);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSerie);
+            this.Name = "Principal";
+            this.Text = "Principal";
+            this.Load += new System.EventHandler(this.Principal_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSerie;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFolio;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ DetalleRemisiones/Forms/Principal.cs
@@ -0,0 +1,101 @@
+﻿using ClosedXML.Excel;
+using DetalleRemisiones.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DetalleRemisiones.Forms
+{
+    public partial class Principal : Form
+    {
+        string rutaLocal;
+        SqlConnection conexion;
+        SqlCommand command;
+        SqlDataAdapter dataAdapter;
+        DataTable dataTable = new DataTable();
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void Principal_Load(object sender, EventArgs e)
+        {
+            this.rutaLocal = Application.StartupPath;
+            InnerDatabaseManager databaseManager = new InnerDatabaseManager();
+            this.conexion = databaseManager.createConnectionFromIniFile(this.rutaLocal);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.dataTable.Clear();
+
+            string serie = this.txtSerie.Text.Trim();
+            string folio = this.txtFolio.Text.Trim();
+
+
+            try
+            {
+                this.command = new SqlCommand("SELECT        admDocumentos.CSERIEDOCUMENTO, admDocumentos.CFOLIO, admDocumentos.CFECHA, " +
+                    "admDocumentos.CRAZONSOCIAL, admDocumentos.CRFC, " +
+                    "SUM(admComponentesPaquete.CCANTIDADPRODUCTO * admMovimientos.CUNIDADES) AS CANTIDADCOMPONENTES, Componentes.CCODIGOPRODUCTO AS CODIGOCOMPONENTE, Componentes.CNOMBREPRODUCTO AS NOMBRECOMPONENTE " +
+                    "FROM            admDocumentos " +
+                    "LEFT JOIN admMovimientos " +
+                    "ON admDocumentos.CIDDOCUMENTO = admMovimientos.CIDDOCUMENTO " +
+                    "LEFT JOIN admProductos AS Paquetes " +
+                    "ON admMovimientos.CIDPRODUCTO = Paquetes.CIDPRODUCTO " +
+                    "LEFT JOIN admComponentesPaquete " +
+                    "ON Paquetes.CIDPRODUCTO = admComponentesPaquete.CIDPAQUETE " +
+                    "LEFT JOIN admProductos AS Componentes " +
+                    "ON admComponentesPaquete.CIDPRODUCTO = Componentes.CIDPRODUCTO " +
+                    "WHERE(admDocumentos.CSERIEDOCUMENTO = @serieDocto AND admDocumentos.CFOLIO = @folioDocto) " +
+                    "GROUP BY         admDocumentos.CSERIEDOCUMENTO, admDocumentos.CFOLIO, admDocumentos.CFECHA, " +
+                    "admDocumentos.CRAZONSOCIAL, admDocumentos.CRFC, " +
+                    "Componentes.CCODIGOPRODUCTO, Componentes.CNOMBREPRODUCTO", this.conexion);
+
+                this.command.Parameters.AddWithValue("@serieDocto", serie);
+                this.command.Parameters.AddWithValue("@folioDocto", folio);
+                this.command.CommandTimeout = 600;
+
+                this.dataAdapter = new SqlDataAdapter(this.command);
+                this.dataTable = new DataTable();
+                this.dataAdapter.Fill(dataTable);
+
+
+                XLWorkbook workBook = new XLWorkbook();
+                workBook.Worksheets.Add(dataTable, "Detalle remision");
+
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Hoja de calculo Excel (*.xlsx)|*.xlsx";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    workBook.SaveAs(saveFileDialog.FileName);
+                    MessageBox.Show("Reporte guardado");
+                }
+                else
+                {
+                    MessageBox.Show("Error generando el reporte");
+                }
+            }
+            catch(SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.conexion.Close();
+            Application.Exit();
+        }
+    }
+}
