21671cad86b8ca7d7941638b18b659bc5a839f85
Eduardo Ascencio Bribiesca <eduardoascbri@outlook.com>
2019-12-16 17:03:03 -0600

    Incorporacion de Productos, Servicios y Componentes de Paquetes

--- DetalleRemisiones/Forms/Principal.cs
+++ DetalleRemisiones/Forms/Principal.cs
@@ -19,7 +19,7 @@ namespace DetalleRemisiones.Forms
         SqlConnection conexion;
         SqlCommand command;
         SqlDataAdapter dataAdapter;
-        DataTable dataTable = new DataTable();
+        
         public Principal()
         {
             InitializeComponent();
@@ -34,27 +34,67 @@ namespace DetalleRemisiones.Forms
 
         private void button2_Click(object sender, EventArgs e)
         {
-            this.dataTable.Clear();
-
             string serie = this.txtSerie.Text.Trim();
             string folio = this.txtFolio.Text.Trim();
+
+            //Creacion de la tabla
+            DataTable reporte = new DataTable();
+            reporte.Columns.Add("Serie");
+            reporte.Columns.Add("Folio");
+            reporte.Columns.Add("Fecha");
+            reporte.Columns.Add("RazonSocial");
+            reporte.Columns.Add("RFC");
+            reporte.Columns.Add("Cantidad");
+            reporte.Columns.Add("Codigo");
+            reporte.Columns.Add("Nombre");
 
 
             try
             {
+                //Productos
                 this.command = new SqlCommand("SELECT        admDocumentos.CSERIEDOCUMENTO, admDocumentos.CFOLIO, admDocumentos.CFECHA, " +
                     "admDocumentos.CRAZONSOCIAL, admDocumentos.CRFC, " +
-                    "SUM(admComponentesPaquete.CCANTIDADPRODUCTO * admMovimientos.CUNIDADES) AS CANTIDADCOMPONENTES, Componentes.CCODIGOPRODUCTO AS CODIGOCOMPONENTE, Componentes.CNOMBREPRODUCTO AS NOMBRECOMPONENTE " +
+                    "SUM(admMovimientos.CUNIDADES) AS CANTIDAD, Productos.CCODIGOPRODUCTO AS CODIGO, Productos.CNOMBREPRODUCTO AS NOMBRE, Productos.CTIPOPRODUCTO, " +
+                    "SUM(admComponentesPaquete.CCANTIDADPRODUCTO * admMovimientos.CUNIDADES) AS CANTIDAD, Componentes.CCODIGOPRODUCTO AS CODIGO, Componentes.CNOMBREPRODUCTO AS NOMBRE " +
                     "FROM            admDocumentos " +
                     "LEFT JOIN admMovimientos " +
                     "ON admDocumentos.CIDDOCUMENTO = admMovimientos.CIDDOCUMENTO " +
-                    "LEFT JOIN admProductos AS Paquetes " +
-                    "ON admMovimientos.CIDPRODUCTO = Paquetes.CIDPRODUCTO " +
+                    "LEFT JOIN admProductos AS Productos " +
+                    "ON admMovimientos.CIDPRODUCTO = Productos.CIDPRODUCTO " +
                     "LEFT JOIN admComponentesPaquete " +
-                    "ON Paquetes.CIDPRODUCTO = admComponentesPaquete.CIDPAQUETE " +
+                    "ON Productos.CIDPRODUCTO = admComponentesPaquete.CIDPAQUETE " +
                     "LEFT JOIN admProductos AS Componentes " +
                     "ON admComponentesPaquete.CIDPRODUCTO = Componentes.CIDPRODUCTO " +
-                    "WHERE(admDocumentos.CSERIEDOCUMENTO = @serieDocto AND admDocumentos.CFOLIO = @folioDocto) " +
+                    "WHERE(admDocumentos.CSERIEDOCUMENTO = @serieDocto AND admDocumentos.CFOLIO = @folioDocto) AND(Productos.CTIPOPRODUCTO != 2) " +
+                    "GROUP BY         admDocumentos.CSERIEDOCUMENTO, admDocumentos.CFOLIO, admDocumentos.CFECHA, " +
+                    "admDocumentos.CRAZONSOCIAL, admDocumentos.CRFC, " +
+                    "Productos.CCODIGOPRODUCTO, Productos.CNOMBREPRODUCTO, Productos.CTIPOPRODUCTO, " +
+                    "Componentes.CCODIGOPRODUCTO, Componentes.CNOMBREPRODUCTO", this.conexion);
+
+                this.command.Parameters.AddWithValue("@serieDocto", serie);
+                this.command.Parameters.AddWithValue("@folioDocto", folio);
+                this.command.CommandTimeout = 600;
+
+                DataTable consultaProductos = new DataTable();
+                this.dataAdapter = new SqlDataAdapter(this.command);
+                consultaProductos = new DataTable();
+                dataAdapter.Fill(consultaProductos);
+
+
+                //Paquetes
+                this.command = new SqlCommand("SELECT        admDocumentos.CSERIEDOCUMENTO, admDocumentos.CFOLIO, admDocumentos.CFECHA, " +
+                    "admDocumentos.CRAZONSOCIAL, admDocumentos.CRFC, " +
+                    "SUM(admComponentesPaquete.CCANTIDADPRODUCTO * admMovimientos.CUNIDADES) AS CANTIDADCOMPONENTE, Componentes.CCODIGOPRODUCTO AS CODIGOCOMPONENTE, Componentes.CNOMBREPRODUCTO AS NOMBRECOMPONENTE " +
+                    "FROM            admDocumentos " +
+                    "LEFT JOIN admMovimientos " +
+                    "ON admDocumentos.CIDDOCUMENTO = admMovimientos.CIDDOCUMENTO " +
+                    "LEFT JOIN admProductos AS Productos " +
+                    "ON admMovimientos.CIDPRODUCTO = Productos.CIDPRODUCTO " +
+                    "LEFT JOIN admComponentesPaquete " +
+                    "ON Productos.CIDPRODUCTO = admComponentesPaquete.CIDPAQUETE " +
+                    "LEFT JOIN admProductos AS Componentes " +
+                    "ON admComponentesPaquete.CIDPRODUCTO = Componentes.CIDPRODUCTO " +
+                    "WHERE(admDocumentos.CSERIEDOCUMENTO = @serieDocto AND admDocumentos.CFOLIO = @folioDocto) AND(Productos.CTIPOPRODUCTO = 2) " +
                     "GROUP BY         admDocumentos.CSERIEDOCUMENTO, admDocumentos.CFOLIO, admDocumentos.CFECHA, " +
                     "admDocumentos.CRAZONSOCIAL, admDocumentos.CRFC, " +
                     "Componentes.CCODIGOPRODUCTO, Componentes.CNOMBREPRODUCTO", this.conexion);
@@ -63,13 +103,41 @@ namespace DetalleRemisiones.Forms
                 this.command.Parameters.AddWithValue("@folioDocto", folio);
                 this.command.CommandTimeout = 600;
 
+                DataTable consultaPaquetes = new DataTable();
                 this.dataAdapter = new SqlDataAdapter(this.command);
-                this.dataTable = new DataTable();
-                this.dataAdapter.Fill(dataTable);
+                consultaPaquetes = new DataTable();
+                dataAdapter.Fill(consultaPaquetes);
 
+                foreach (DataRow row in consultaProductos.Rows)
+                {
+                    DataRow dataRow = reporte.NewRow();
+                    dataRow["Serie"] = row["CSERIEDOCUMENTO"];
+                    dataRow["Folio"] = row["CFOLIO"];
+                    dataRow["Fecha"] = row["CFECHA"];
+                    dataRow["RazonSocial"] = row["CRAZONSOCIAL"];
+                    dataRow["RFC"] = row["CRFC"];
+                    dataRow["Cantidad"] = row["CANTIDAD"];
+                    dataRow["Codigo"] = row["CODIGO"];
+                    dataRow["Nombre"] = row["NOMBRE"];
+                    reporte.Rows.Add(dataRow);
+                }
+
+                foreach (DataRow row in consultaPaquetes.Rows)
+                {
+                    DataRow dataRow = reporte.NewRow();
+                    dataRow["Serie"] = row["CSERIEDOCUMENTO"];
+                    dataRow["Folio"] = row["CFOLIO"];
+                    dataRow["Fecha"] = row["CFECHA"];
+                    dataRow["RazonSocial"] = row["CRAZONSOCIAL"];
+                    dataRow["RFC"] = row["CRFC"];
+                    dataRow["Cantidad"] = row["CANTIDADCOMPONENTE"];
+                    dataRow["Codigo"] = row["CODIGOCOMPONENTE"];
+                    dataRow["Nombre"] = row["NOMBRECOMPONENTE"];
+                    reporte.Rows.Add(dataRow);
+                }
 
                 XLWorkbook workBook = new XLWorkbook();
-                workBook.Worksheets.Add(dataTable, "Detalle remision");
+                workBook.Worksheets.Add(reporte, "Detalle remision");
 
                 SaveFileDialog saveFileDialog = new SaveFileDialog();
                 saveFileDialog.Filter = "Hoja de calculo Excel (*.xlsx)|*.xlsx";
@@ -87,9 +155,6 @@ namespace DetalleRemisiones.Forms
             {
                 MessageBox.Show(ex.Message);
             }
-            
-            
-
         }
 
         private void button1_Click(object sender, EventArgs e)
